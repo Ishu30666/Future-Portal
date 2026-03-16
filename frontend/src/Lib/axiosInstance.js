0051bbbffce32b4341a55940c0d96470e19e9d96
Ishu <ishu30666@gmail.com>
2026-03-16 13:12:13 +0530

    Update axiosInstance.js

--- frontend/src/Lib/axiosInstance.js
+++ frontend/src/Lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://future-portal-production.up.railway.app/api/",
+  baseURL: "https://edisha-gov.info/api/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
